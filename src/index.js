f3a77b378c744f32b63704473f43b9eca39a0bc5
Diana Marcelin <dianamarcelin@Dianas-MacBook-Air.local>
2022-01-31 17:10:29 -0500

    created a snapshot of collection in fs

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
+const { response } = require("express");
 const express = require("express");
+const { initializeApp, cert } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
+
+const credentials = require("../credentials.json");
+
+initializeApp({
+  credential: cert(credentials),
+});
+
+const db = getFirestore();
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/", (request, response) => {
+  const userCollection = db.collection("users");
+
+  userCollection.get().then((snapshot) => {
+    // const users = []
+    // snapshot.forEach(doc => {
+    //     users.push({id: doc.id, ...doc.data()})
+    // })
+
+    response.send(snapshot.docs);
+  });
+
+  // response.send('Hello World!');
 });
 
 app.post("/users", (req, res) => {
@@ -14,6 +36,11 @@ app.post("/users", (req, res) => {
   //^const age1 = req.body.age;
   //^const email1= req.body.email
   const user = { name, age, email }; //want to create an object with these variable----the key of these properties is name, age, and email
+  //  ^ const user= {
+  //       name: name,
+  //       age: age,
+  //       email:
+  //   }
   const result = `My name is ${user.name}, I am ${user.age} years old and my email is ${user.email}`;
   res.send(result);
 });
